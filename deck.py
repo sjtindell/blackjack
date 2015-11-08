c6187aa053ec7fc388dddcf8eac29e11218559b9
sam t <sjtindell@gmail.com>
2015-11-07 21:16:44 -0800

    Improved string output for win decision

--- deck.py
+++ deck.py
@@ -4,13 +4,30 @@ import random
 class Deck(object):
   
   def __init__(self):
-    self.deck = [] 
+    self.deck = []
+    self.replenish()
+    self.shuffle()
+  
+  def __repr__(self):
+    return " ".join(str(i) for i in self.deck)
+
+  def replenish(self):
     contains = ["A", "K", "Q", "J", 2, 3, 4, 5, 6, 7, 8, 9, 10]
     self.deck = [card for x in range(4) for card in contains]
+    self.shuffle()
+    
 
   def shuffle(self):
     random.shuffle(self.deck)
 
   def deal(self, n):
-    return [self.deck.pop() for _ in range(n)]
+    try:
+      cards = [self.deck.pop() for _ in range(n)]
+    except IndexError:
+      print("deck exhausted")
+      print("deck: ", self.deck)
+      self.replenish()
+      cards = [self.deck.pop() for _ in range(n)]
+    return cards
+      
 
